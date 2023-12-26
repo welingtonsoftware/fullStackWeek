dd35ec5767b2e7c87911c853977ec1a458c85a8b
welingtonsoftware <dpepsloco@gmail.com>
2023-12-26 15:11:03 -0300

    feat: ensure product quantity can be modified on

--- src/components/ui/cart-item.tsx
+++ src/components/ui/cart-item.tsx
@@ -1,27 +1,37 @@
-import { CartProduct } from "@/providers/cart";
+import { CartContext, CartProduct } from "@/providers/cart";
 import Image from "next/image";
 import { Button } from "./button";
-import { ArrowLeftIcon, ArrowRightIcon, TractorIcon, TrashIcon } from "lucide-react";
+import {
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  TractorIcon,
+  TrashIcon,
+} from "lucide-react";
+import { useContext } from "react";
 
 interface CartItemProps {
   product: CartProduct;
 }
 const CartItem = ({ product }: CartItemProps) => {
+  const { decreaseProductQuantity } = useContext(CartContext);
+
+  const handleDecreaseProductQuantityClick = () => {
+    decreaseProductQuantity(product.id);
+  };
+
   return (
-
-      <div className="flex items-center justify-between">
-        <div className="flex items-center gap-4">
-
-          <div className="flex items-center justify-center rounded-lg bg-accent h-[77px] w-[77px]">
-            <Image
-              src={product.imageURLs[0]}
-              width={0}
-              height={0}
-              sizes="100vw"
-              alt={product.name}
-              className="w-auto h-auto max-h-[70%] max-x-[80%]"
-            />
-          </div>
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-4">
+        <div className="flex items-center justify-center rounded-lg bg-accent h-[77px] w-[77px]">
+          <Image
+            src={product.imageURLs[0]}
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt={product.name}
+            className="w-auto h-auto max-h-[70%] max-x-[80%]"
+          />
+        </div>
 
         <div className="flex flex-col">
           <p className="text-xs">{product.name}</p>
@@ -37,7 +47,12 @@ const CartItem = ({ product }: CartItemProps) => {
             )}
           </div>
           <div className="flex items-center gap-1">
-            <Button size="icon" variant="outline" className="h-8 w-8">
+            <Button
+              size="icon"
+              variant="outline"
+              className="h-8 w-8"
+              onClick={handleDecreaseProductQuantityClick}
+            >
               <ArrowLeftIcon size={16} />
             </Button>
             <span className="text-xs"> {product.quantity}</span>
--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -14,6 +14,7 @@ interface ICartContext {
   cartBasePrice: number;
   cartTotalDiscount: number;
   addProductToCart: (product: CartProduct) => void;
+  decreaseProductQuantity: (productId: string) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
@@ -22,19 +23,55 @@ export const CartContext = createContext<ICartContext>({
   cartBasePrice: 0,
   cartTotalDiscount: 0,
   addProductToCart: () => {},
+  decreaseProductQuantity: () => {},
 })
 
 const CartProvider = ({children}: {children: ReactNode}) => {
   const [products, setProducts] = useState<CartProduct[]>([])
 
   const addProductToCart = (product: CartProduct) => {
+    const productIsAlreadyOnCart = products.some(
+      cartProduct => cartProduct.id == product.id,
+      );
+
+    if (productIsAlreadyOnCart){
+      setProducts((prev) => 
+        prev.map((cartProduct) => {
+        if (cartProduct.id == product.id) {
+          return{
+            ...cartProduct,
+            quantity: cartProduct.quantity + 1,
+          };
+        }
+        return cartProduct;
+      }),
+      );
+      return;
+    }
+
     setProducts((prev) => [...prev, product])
   }
+
+  const decreaseProductQuantity = (productId: string) => {
+
+    setProducts(prev => prev.map(cartProduct => {
+      if (cartProduct.id == productId){
+        return{
+          ... cartProduct,
+          quantity: cartProduct.quantity - 1
+        }
+      }
+      return cartProduct
+    }).filter((cartProduct) => cartProduct.quantity > 0),
+    )
+  }
+
   return (
     <CartContext.Provider
     value={{
       products,
       addProductToCart,
+      decreaseProductQuantity,
       cartTotalPrice: 0,
       cartBasePrice: 0,
       cartTotalDiscount: 0,
